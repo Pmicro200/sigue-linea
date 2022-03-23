702309ab2d83991200c7dc28f248408b0b5176c6
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-03-23 09:25:39 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ basic.forever(function () {
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 140)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 140)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
