2a37b3b7243b8b0a51a9f95c4cb6a43e5c559c22
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-03-27 17:08:16 +0200

    cghfcyvghvhjyv

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 140)
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 140)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 140)
     } else {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 140)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 140)
